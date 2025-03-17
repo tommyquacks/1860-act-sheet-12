e58e1b7b391d4272a80c6e70a1f719ad3c30d7df
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2025-03-17 16:59:47 +0000

    Update SumArrayEntries.asm

--- SumArrayEntries.asm
+++ SumArrayEntries.asm
@@ -1,5 +1,5 @@
 // SumArrayEntries.asm
-// Computes sum of array elements
+// Computes the sum of array elements
 
 @R1
 D=M
@@ -8,9 +8,8 @@ D;JLE       // If n <= 0, return 0
 
 @R2
 M=0         // Initialize sum = 0
-
 @R3
-M=0         // Index = 0
+M=0         // Initialize index = 0
 
 (LOOP)
 @R3
@@ -21,25 +20,25 @@ D=D-M
 D;JGE       // If index >= n, exit loop
 
 @R0
-D=M        // Base address of array
+D=M        // Load base address of the array
 @R3
-A=D+M      // Compute memory address of A[i]
-D=M       // Load A[i] value
+A=D+M      // Compute address of A[i]
+D=M        // Load A[i] value
 @R2
-M=M+D     // sum += A[i]
+M=M+D      // sum += A[i]
 
 @R3
-M=M+1     // index++
+M=M+1      // index++
 
 @LOOP
-0;JMP     // Repeat
+0;JMP      // Repeat
 
 (END)
 @END
-0;JMP     // Halt execution
+0;JMP      // Halt execution
 
 (INVALID)
 @R2
-M=0       // Return 0 if n <= 0
+M=0        // Return 0 if n <= 0
 @END
 0;JMP
