a130e9b4122501ae3b213aeb684f24ef570085eb
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2025-03-17 15:47:08 +0000

    Create Square.asm

---
+++ Square.asm
@@ -0,0 +1,20 @@
+// Square.asm
+// Computes y = x^2 and stores in R1
+
+@R0
+D=M         // D = x
+
+@R1
+M=0         // Initialize R1 = 0 (y = 0)
+
+@LOOP
+D;JEQ       // If x == 0, exit loop
+
+@R1
+M=M+D       // y = y + x
+
+D=D-1       // Decrease counter
+
+@LOOP
+D;JGT       // Repeat while x > 0
+
