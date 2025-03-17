a17c9204e28f32b9cfeeb54e688e7cb689c057fd
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2025-03-17 15:48:58 +0000

    Create SumArrayEntries.asm

---
+++ SumArrayEntries.asm
@@ -0,0 +1,40 @@
+// SumArrayEntries.asm
+// Computes sum of array elements
+
+@R1
+D=M
+@INVALID
+D;JLE       // If n <= 0, return 0
+
+@R2
+M=0         // Initialize sum = 0
+
+@R3
+M=0         // Index = 0
+
+(LOOP)
+@R3
+D=M
+@R1
+D=D-M
+@END
+D;JGE       // If index >= n, exit loop
+
+@R0
+D=M
+@R3
+A=D+M       // Address of A[i]
+
+D=M
+@R2
+M=M+D       // sum += A[i]
+
+@R3
+M=M+1       // index++
+
+@LOOP
+0;JMP       // Repeat
+
+(INVALID)
+@R2
+M=0         // Return 0
